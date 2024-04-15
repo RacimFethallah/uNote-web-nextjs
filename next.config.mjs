8b0861144f82bc4d2f687c2ebdd1df8d975d08a9
racimfethallah <ricdev.io@gmail.com>
2024-04-15 21:47:02 +0100

    d

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,4 @@
-// next.config.js
-module.exports = {
-    webpack(config) {
-        config.resolve.fallback = {
-            fs: false,
-            path: false,
-        };
-        config.module.rules.push({
-            test: /\.html$/,
-            use: 'ignore-loader',
-        });
-        return config;
-    },
-};
+/** @type {import('next').NextConfig} */
+const nextConfig = {};
+
+export default nextConfig;
